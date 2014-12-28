700338191236567f0e3b545ba646355781dd43e5
Anthony Mayer <adwmayer@gmail.com>
2014-12-28 21:57:30 +0100

    updating repo name
    And removing meteorite installation

--- configure.sh
+++ configure.sh
@@ -2,11 +2,8 @@
 
 #configuring the system
 wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/Makefile
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/start_test.js
+wget https://raw.github.com/anthonymayer/travis-ci-meteor-packages/master/start_test.js
 wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/phantom_runner.js
 
 #install meteor
 curl https://install.meteor.com | /bin/sh
-
-#installing meteorite
-npm install -g meteorite
