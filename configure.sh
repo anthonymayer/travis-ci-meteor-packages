dd1a26015f5f6bfc098f64240bbe2e60523f3353
Anthony Mayer <adwmayer@gmail.com>
2014-12-29 11:36:41 +0100

    Putting scripts in dir

--- configure.sh
+++ configure.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+mkdir -p .scripts
+
+cd .scripts/
+
 #configuring the system
 wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/Makefile
 wget https://raw.github.com/anthonymayer/travis-ci-meteor-packages/master/start_test.js
 wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/phantom_runner.js
+
+cd -
 
 #install meteor
 curl https://install.meteor.com | /bin/sh
